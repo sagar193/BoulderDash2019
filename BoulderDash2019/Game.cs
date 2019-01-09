18e496fff9a0b6c36fde0b05bdb167c40270d153
Sagar Gangabisoensingh <sagartje1993@gmail.com>
2019-01-09 21:18:30 +0100

    A new start

---
+++ BoulderDash2019/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoulderDash2019
+{
+    class Game
+    {
+        Level[] levels;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.ReadKey();
+
+            Game game = new Game();
+        }
+
+        public Game()
+        {
+            levels = new Level[3];
+            MapLoader mapLoader = new MapLoader();
+            levels[0] = mapLoader.createLevel(LevelData.Level1);
+        }
+    }
+}
---
+++ BoulderDash2019/mapLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoulderDash2019
+{
+    class MapLoader
+    {
+        internal Level createLevel(char[,] level1)
+        {
+            for (int i = 0; i < level1.GetLength(0); i++)
+            {
+                for (int j = 0; j < level1.GetLength(1); j++)
+                {
+
+                }
+            }
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
